b47205e845d2b6ce18a1f4ac961b7420d679c366
Eleanor Weigert <eleanor.weigert@byhook.com>
2016-02-10 17:30:30 -0800

    Add edit/update functionality

--- app/views/todo/todo.js
+++ app/views/todo/todo.js
@@ -12,6 +12,7 @@ var Todo = Backbone.View.extend({
 
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
+    this.input = this.$('.edit');
     return this;
   },
 
@@ -22,11 +23,33 @@ var Todo = Backbone.View.extend({
 
   events: {
     'click [type="checkbox"]':'toggleDone',
-    'click .destroy': 'destroy'
+    'click .destroy': 'destroy',
+    'dblclick .todo': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit': 'close'
   },
 
   toggleDone: function() {
     this.model.toggle();
+  },
+
+  edit: function() {
+    this.$el.addClass('editing');
+    this.input.focus();
+  },
+
+  close: function() {
+    var value = this.input.val().trim();
+    if (value) {
+      this.model.save({label: value});
+    }
+    this.$el.removeClass('editing');
+  },
+
+  updateOnEnter: function(e) {
+    if (e.which === 13) {
+      this.close();
+    }
   },
 
   destroy: function() {
